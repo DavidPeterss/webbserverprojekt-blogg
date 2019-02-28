3b036cde9b41037ed521455cf5effd6534e0ee2c
itggot-david-petersson <david.petersson@itggot.se>
2019-02-28 11:05:17 +0100

    Tried to make posts a thing

--- Blogg/app.rb
+++ Blogg/app.rb
@@ -18,6 +18,23 @@ get('/register') do
     slim(:register)
 end
 
+get('/failed') do
+    slim(:failed)
+end
+
+post('/login') do
+    db = SQLite3::Database.new("db/användare.db")
+    db.results_as_hash = true
+    user_password = db.execute("SELECT Password FROM users WHERE Username = ?", params["username"])
+    hashed_pass = BCrypt::Password.new(user_password[0]["Password"])
+    if hashed_pass == params["password"]
+         session[:user] = hashed_pass
+    else
+        redirect('/failed')
+    end
+    redirect('/loggedin')
+end
+
 post('/register') do
     db = SQLite3::Database.new("db/användare.db")
     db.results_as_hash = true
@@ -25,4 +42,10 @@ post('/register') do
 
     db.execute("INSERT INTO users(Username, Password) VALUES(?, ?)", params["username"], hashed_pass)
     redirect('/')  
+end
+
+post('/text') do
+    db = SQLite3::Database.new("db/användare.db")
+    db.execute("INSERT INTO bloggposts(Posts, Header) VALUES(?, ?)", params["bloggpost"], params["rubrik"])
+    redirect('/loggedin')
 end
